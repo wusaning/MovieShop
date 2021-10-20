bcd9da5ed5ffbe58c28ccf6a7ad7697967245901
Yiming <90655469+yiming-2021@users.noreply.github.com>
2021-10-20 17:15:27 -0400

    Add files via upload

---
+++ ApplicationCore/Entities/Cast.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    [Table("Cast")]
+    public class Cast
+    {
+        public int Id { get; set; }
+       
+        [MaxLength(60)]
+        public string Name { get; set; }
+        [MaxLength(120)]
+        public string TmdbUrl { get; set; }
+        [MaxLength(120)]
+        public string ProfilePath { get; set; }
+    }
+}
+
+
---
+++ ApplicationCore/Entities/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    [Table("Genre")]
+    public class Genre
+    {
+        public int Id { get; set; } // attribute named 'Id' will be automatically set PK by default of migration
+
+        [MaxLength(24)]
+        public string Name { get; set; }
+    }
+}
